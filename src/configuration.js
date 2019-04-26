736d2c2b10b55cd8048e7cfcb4f96f450b7887a6
Georman Calderon <georve@gmail.com>
2019-04-26 19:42:15 -0400

    cuenta de correo

--- src/configuration.js
+++ src/configuration.js
@@ -9,4 +9,6 @@ else {
 	urlDB = 'mongodb+srv://georve:oPaEv7YOmgWt39mq@nodecursejs-lddv7.mongodb.net/estudiantes?retryWrites=true'
 }
 
-process.env.URLDB = urlDB
+process.env.URLDB = urlDB;
+
+process.env.SENGRID_API_KEY='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'
